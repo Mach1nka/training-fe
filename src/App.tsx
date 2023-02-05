8a2a6662ade4fe7262b71eaccd48c5b6d3403d1c
Mach1nka <uspenskiyart@gmail.com>
2023-02-05 18:52:41 +0300

    init: set up project structure

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Suspense } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
+import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
+import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
+import { useTheme } from "./theme/useTheme";
+import { classNames } from './utils/classNames/classNames';
+classNames('remove-btn', {hovered: false, selectable: true}, ['add']);
+import './styles/index.scss';
+
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={classNames('app', {}, [theme])}>
+      <button onClick={toggleTheme}>
+        Switch theme
+      </button>
+      <Link to={'/'}>Main Page</Link>
+      <Link to={'/about'}>About Page</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageLazy />} />
+          <Route path={'/'} element={<MainPageLazy />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Example.tsx
@@ -0,0 +1,7 @@
+import classes from './Example.module.scss';
+
+export const Example = () => {
+  return (
+    <button className={classes.button}>Click me</button>
+  );
+}
