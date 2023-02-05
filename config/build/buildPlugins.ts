dde42b469070b8c05f45d9a922d0d3a5930eb2cd
Mach1nka <uspenskiyart@gmail.com>
2023-02-05 18:54:47 +0300

    init: add config

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,16 @@
+import { ProgressPlugin, WebpackPluginInstance } from 'webpack';
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+import { BuildOptions } from './types/config';
+
+export function buildPlugins({paths}: BuildOptions): WebpackPluginInstance[] {
+  return [
+    new HTMLWebpackPlugin({ template: paths.html }),
+    new MiniCssExtractPlugin({ 
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css'
+    }),
+    new ProgressPlugin(),
+  ];
+}
