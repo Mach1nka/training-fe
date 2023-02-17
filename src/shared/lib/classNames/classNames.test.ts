014a4b5812f7f36b961bcbc8c6224d61c8873e44
Mach1nka <uspenskiyart@gmail.com>
2023-02-18 02:26:06 +0300

    feat: integrate jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,22 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+  test('class name', () => {
+    expect(classNames('someClass')).toBe('someClass');
+  });
+
+  test('additional class', () => {
+    const expectedResult = 'someClass class1 class2';
+    expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expectedResult);
+  });
+
+  test('class mods', () => {
+    const expectedResult = 'someClass hovered clickable';
+    expect(classNames('someClass', { hovered: true, clickable: true }, [])).toBe(expectedResult);
+  });
+
+  test('class mods', () => {
+    const expectedResult = 'someClass class1 class2 hovered';
+    expect(classNames('someClass', { hovered: true, clickable: false }, ['class1', 'class2'])).toBe(expectedResult);
+  });
+});
