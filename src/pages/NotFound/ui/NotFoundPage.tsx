351f027c779eb596a378ee77c949e4e0c69c7644
Mach1nka <uspenskiyart@gmail.com>
2023-02-18 03:06:24 +0300

    feat: add loader nad not found page

---
+++ src/pages/NotFound/ui/NotFoundPage.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { classNames } from '@/shared/lib/classNames/classNames';
+
+import cls from './NotFoundPage.module.scss';
+
+interface Props {
+  className?: string;
+}
+
+export const NotFoundPage: FC<Props> = ({ className }) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className={classNames(cls.NotFoundPage, {}, [className])}>
+      {t('pageNotFound')}
+    </div>
+  );
+};
---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+
+import { classNames } from '@/shared/lib/classNames/classNames';
+
+import cls from './Loader.module.scss';
+
+interface Props {
+  className?: string;
+}
+
+export const Loader: FC<Props> = ({ className }) => (
+  <div className={classNames(cls.Loader, {}, [className])}>
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+);
