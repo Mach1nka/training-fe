2919c32c57c1c9e8e7821c250b0aa360629cc4fa
Mach1nka <uspenskiyart@gmail.com>
2024-02-21 21:03:39 +0300

    fix: sync package lock

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -28,7 +28,7 @@ export default ({ config }: {config: Configuration}) => {
     {
       test: /\.s[ac]ss$/i,
       use: [
-      // Creates `style` nodes from JS strings
+        // Creates `style` nodes from JS strings
         'style-loader',
         // Translates CSS into CommonJS
         {
